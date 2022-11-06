c0d4025029f12a65b40ab75e075ff79bfaf03adf
Sergey Lebed <swansergey7@gmail.com>
2022-11-06 14:09:05 +0200

    review section ends

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,6 @@
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.header .navbar');
 
-
 menu.onclick = () => {
 	menu.classList.toggle('fa-times');
 	navbar.classList.toggle('active');
@@ -17,12 +16,29 @@ var swiper = new Swiper(".home-slider", {
 	spaceBetween: 20,
 	effect: "fade",
 	grabCursor: true,
-	loop:true,
+	loop: true,
 	centeredSlides: true,
 	pagination: {
-	  el: ".swiper-pagination",
-	  clickable: true,
+		el: ".swiper-pagination",
+		clickable: true,
 	},
 });
 
+var swiper = new Swiper(".review-slider", {
+	spaceBetween: 20,
+	grabCursor: true,
+	loop: true,
+	autoplay: {
+		delay: 7500,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		0: {
+			slidesPerView: 1,
+		},
+		600: {
+			slidesPerView: 2,
+		},
+	},
+});
 
